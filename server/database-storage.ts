3c4e45ed3b94c435c4ca8d971dd80cb7403b56fd
aliabdalla2750 <aliabdalla2750@gmail.com>
2025-05-01 22:10:46 +0300

    Update database-storage.ts

--- server/database-storage.ts
+++ server/database-storage.ts
@@ -1,6 +1,6 @@
 import { eq, and, desc, isNull } from "drizzle-orm";
 import { db } from "./db";
-import {
+-import {
   users, User, InsertUser,
   agencies, Agency, InsertAgency,
   clients, Client, InsertClient,
